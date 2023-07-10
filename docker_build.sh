4d2a770da11c19c999e078d55f95ac56260491f1
cubean <cubean@warp-driven.com>
2023-07-10 18:07:38 +1000

    added docker push

--- docker_build.sh
+++ docker_build.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
-docker build -t warpdriven/wd-erp-16:latest .
+
+# Get the current date in the desired format (YYYYMMDD)
+tag_today=$(date +'%Y%m%d')
+docker build -t wd-erp-16:$tag_today .
+
+docker tag wd-erp-16:$tag_today warpdriven/wd-erp-16:latest
+docker tag wd-erp-16:$tag_today warpdriven/wd-erp-16:$tag_today
+
+# Log in to Docker Hub
+docker login
 
 #docker run --name wd-erp-test warpdriven/wd-erp:latest
+
+# Push the Docker image to Docker Hub
+docker push warpdriven/wd-erp-16:$tag_today
+docker push warpdriven/wd-erp-16:latest
