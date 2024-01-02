52b6a82858e52a5ea3638ebbe1478e466a4667ef
cubean <cubean@warp-driven.com>
2024-01-02 23:46:33 +1100

    added isc 16.0 version

--- docker_build_enterprise.sh
+++ docker_build_enterprise.sh
@@ -3,16 +3,16 @@
 
 # Get the current date in the desired format (YYYYMMDD)
 tag_today=$(date +'%Y%m%d')
-docker build -t wd-erp-16:$tag_today .
+docker build --platform="linux/amd64" -t wd-enterprise-16:$tag_today .
 
-docker tag wd-erp-16:$tag_today warpdriven/wd-erp-16:latest
-docker tag wd-erp-16:$tag_today warpdriven/wd-erp-16:$tag_today
+docker tag wd-enterprise-16:$tag_today warpdriven/wd-enterprise-16:latest
+docker tag wd-enterprise-16:$tag_today warpdriven/wd-enterprise-16:$tag_today
 
 # Log in to Docker Hub
 docker login
 
-#docker run --name wd-erp-test warpdriven/wd-erp:latest
+#docker run --name wd-enterprise-test warpdriven/wd-erp:latest
 
 # Push the Docker image to Docker Hub
-docker push warpdriven/wd-erp-16:$tag_today
-docker push warpdriven/wd-erp-16:latest
+docker push warpdriven/wd-enterprise-16:$tag_today
+docker push warpdriven/wd-enterprise-16:latest
--- docker_build_isc.sh
+++ docker_build_isc.sh
@@ -3,7 +3,7 @@
 
 # Get the current date in the desired format (YYYYMMDD)
 tag_today=$(date +'%Y%m%d')
-docker build -t wd-isc-16:$tag_today .
+docker build --platform="linux/amd64" -t wd-isc-16:$tag_today .
 
 docker tag wd-isc-16:$tag_today warpdriven/wd-isc-16:latest
 docker tag wd-isc-16:$tag_today warpdriven/wd-isc-16:$tag_today
